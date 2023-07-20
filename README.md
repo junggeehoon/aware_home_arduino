7f3abc487ffe7630fc8e1c6f88b2b567fbfa77fd
junggeehoon <junggeehoon@gmail.com>
2023-07-20 14:38:57 -0400

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # aware_home_arduino
+
+## Overview
+This project forms a part of [The Aware Home](https://gvu.gatech.edu/research/labs/aware-home-research-initiative) project, carried out during the [2023 IPAT Summer Internship Program](https://research.gatech.edu/research-interns-selected-summer-2023). Detailed documentation regarding this project can be found on this [Notion](https://geehoon.notion.site/Machine-Learning-Approaches-for-Indoor-Location-Fingerprinting-Using-Apple-Watch-RSSI-fc6cbe1d59e44aa1a66004446faf7bb2?pvs=4) page.
+The system scans Bluetooth signals emitted by an Apple Watch to determine its location. Various prediction models, including Random Forest, Support Vector Machine, and K-Nearest Neighbors, were utilized.
+
+## Project Directory
+1. **BLE_scan**: Contains file for bluetooth scan.
+2. **Receiver**: Contains file for receiver.
+
+
+## Setup
+
+### Sender
+open the file located at `./Sender/sender.ino` and update the `myData.id` on line 28 to align with the appropriate sensor ID. Once you've made these changes, proceed to upload the sketch to your ESP32.
+Change `broadcastAddress` to align with the appropriate receiver's address if needed.
+
+
+### Receiver
+1. Open the file located at `./Receiver/receiver.ino` and upload the sketch to ESP32.
--- Sender/Sender.ino
+++ Sender/Sender.ino
@@ -1,33 +1,70 @@
+#include <BLEDevice.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#define BOARD_LED 2
+using namespace std;
 
-// REPLACE WITH YOUR RECEIVER MAC Address
 uint8_t broadcastAddress[] = {0xF4, 0xCF, 0xA2, 0x89, 0x8D, 0x70};
 
-// Structure example to send data
-// Must match the receiver structure
+int scanTime = 1; //In seconds
+BLEScan* pBLEScan;
+
 typedef struct struct_message {
-  char a[32];
-  int b;
   int id;
+  int rssi;
 } struct_message;
 
-// Create a struct_message called myData
 struct_message myData;
 
 esp_now_peer_info_t peerInfo;
 
+bool checkAppleWatch(uint8_t cManufacturerData[]) {
+  return cManufacturerData[0] == 0x4C && cManufacturerData[1] == 0x00 && cManufacturerData[2] == 0x10 && cManufacturerData[3] == 0x05 && cManufacturerData[5] == 0x98;
+}
+
+void sendData(int rssi) {
+  myData.id = 10;
+  myData.rssi = rssi;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    digitalWrite(BOARD_LED, HIGH);
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+}
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+
+    std::string strManufacturerData = advertisedDevice.getManufacturerData();
+    uint8_t cManufacturerData[100];
+    strManufacturerData.copy((char *)cManufacturerData, strManufacturerData.length(), 0);
+
+    if (checkAppleWatch(cManufacturerData)) {
+      int rssi = advertisedDevice.getRSSI();
+      // Serial.println(rssi);
+      sendData(rssi);
+      return;
+    }
+  }
+};
+
 // callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status != ESP_NOW_SEND_SUCCESS) {
+    digitalWrite(BOARD_LED, LOW);
+  }
+  // Serial.print("\r\nLast Packet Send Status:\t");
+  // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
- 
+
 void setup() {
-  // Init Serial Monitor
+  pinMode(BOARD_LED,OUTPUT);
   Serial.begin(115200);
- 
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
@@ -36,8 +73,6 @@ void setup() {
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
   
   // Register peer
@@ -50,22 +85,17 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);
 }
- 
+
 void loop() {
-  // Set values to send
-  strcpy(myData.a, "THIS IS A CHAR");
-  myData.b = random(1,20);
-  myData.id = 1;
-  
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
-    Serial.println("Error sending the data");
-  }
-  delay(2000);
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  pBLEScan->clearResults();
+  delay(800);
 }
